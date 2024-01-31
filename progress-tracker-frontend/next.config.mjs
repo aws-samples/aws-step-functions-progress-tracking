7f14a51c64c4967fe3d5e88e2d61bc6bfe358b4a
Alex Paramonov <25197861+alparamonov@users.noreply.github.com>
2024-01-31 22:19:08 +0100

    Fix for amplify ci cd

--- progress-tracker-frontend/next.config.mjs
+++ progress-tracker-frontend/next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    unoptimized: true,
+  },
+  output: "export"
+};
 
 export default nextConfig;
