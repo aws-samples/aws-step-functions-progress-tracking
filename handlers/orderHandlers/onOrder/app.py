ca0e876d15a7fd23c51cb28f2df69ad587a75d11
Raj Jain <jinraj@amazon.com>
2026-05-25 12:30:17 +0530

    PR test

--- handlers/orderHandlers/onOrder/app.py
+++ handlers/orderHandlers/onOrder/app.py
@@ -8,6 +8,8 @@ stepfunctions_arn = os.environ['STEP_FUNCTIONS_ARN']
 def handler(event, context):
     connectionId = event['requestContext']['connectionId']
 
+    print("Calling client.start_execution")
+
     client.start_execution(
         stateMachineArn=stepfunctions_arn,
         input=json.dumps({'ConnectionId': connectionId})
